4efd80ce8e2eb3ee20c741ea39097472d2175783
Piotr Glazar <piotr.glazar@syncron.com>
2013-08-22 11:31:58 +0200

    BPP-9642 Throwable.toString detector
    Signed-off-by: Piotr Findeisen <piotr.findeisen@syncron.com>

---
+++ src/test/java/com/syncron/findbugsextensions/DefaultThrowableToString.java
@@ -0,0 +1,10 @@
+package com.syncron.findbugsextensions;
+
+public class DefaultThrowableToString extends RuntimeException {
+	private static final long serialVersionUID = -566192084666763690L;
+
+	@Override
+	public String getMessage() {
+		return "I'm just overriding getMessage()";
+	}
+}
